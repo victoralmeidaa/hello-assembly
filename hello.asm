9932960545b6a1d0d566173a0389c9ee9c83780f
Victor Almeida <32881893+victoralmeidaa@users.noreply.github.com>
2021-09-30 21:33:17 -0300

    Add files via upload

---
+++ hello.asm
@@ -0,0 +1,26 @@
+section .data		;Define as variáveis na seção de Dados
+	msg: db "Ola mundo", 10 
+	len: equ $-msg
+section .text		;O codigo inicia na seção de Texto
+	global main
+main:				;ssize_t write(int 'fd.' const void '*buf.' size_t 'count')
+	mov	eax, 4		;'write' sistema de chamada = 4 
+	mov	ebx, 1		; (fd) file descriptor
+	mov	ecx, msg	; string para escrita (*buf)
+	mov	edx, len		; comprimento da string para ecrita (count)
+	int	0x80		; chama o kernel
+
+	; Termina o Programa	
+	mov	eax, 1		;'exit' system call
+	mov	ebx, 0		;exit com erro codigo 0
+	int 0x80		;chama o kernel
+;--------------------
+; # Compile
+; nasm -f elf64 hello.nasm -o hello.o
+; # Link
+; gcc hello.o -o hello -no-pie
+; # Run
+; ./hello
+; -------------------
+
+
